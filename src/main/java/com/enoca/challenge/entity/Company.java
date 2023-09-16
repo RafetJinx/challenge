94635bbff4ccf5890a086f2ec8fa93ab3e261db2
Rafet Ersoy <63067602+RafetJinx@users.noreply.github.com>
2023-09-16 21:01:48 +0300

    Entity(model) classes are filled

--- src/main/java/com/enoca/challenge/entity/Company.java
+++ src/main/java/com/enoca/challenge/entity/Company.java
@@ -1,4 +1,27 @@
 package com.enoca.challenge.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "company")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column (name = "name")
+    private String name;
+
+    @Column (name = "address")
+    private String address;
+
+    @Column (name = "phone_number")
+    private String phoneNumber;
 }
--- src/main/java/com/enoca/challenge/entity/Employee.java
+++ src/main/java/com/enoca/challenge/entity/Employee.java
@@ -1,4 +1,33 @@
 package com.enoca.challenge.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "employee")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
+    // define fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="surname")
+    private String surname;
+
+    @Column(name = "email")
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id", referencedColumnName = "id")
+    private Company company;
 }
