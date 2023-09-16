a68dff41dc520dfaf936f75d7bca90449f7c06b9
Rafet Ersoy <63067602+RafetJinx@users.noreply.github.com>
2023-09-16 22:37:03 +0300

    added services

--- src/main/java/com/enoca/challenge/service/abstracts/CompanyService.java
+++ src/main/java/com/enoca/challenge/service/abstracts/CompanyService.java
@@ -1,4 +1,16 @@
 package com.enoca.challenge.service.abstracts;
 
+import com.enoca.challenge.core.utilities.results.dataResult.DataResult;
+import com.enoca.challenge.entity.Company;
+
+import java.util.List;
+
 public interface CompanyService {
+    DataResult<List<Company>> findAll();
+
+    DataResult<Company> findById(int theId);
+
+    DataResult<Company> save(Company company);
+
+    DataResult<String> deleteById(int id);
 }
--- src/main/java/com/enoca/challenge/service/abstracts/EmployeeService.java
+++ src/main/java/com/enoca/challenge/service/abstracts/EmployeeService.java
@@ -1,4 +1,16 @@
 package com.enoca.challenge.service.abstracts;
 
+import com.enoca.challenge.core.utilities.results.dataResult.DataResult;
+import com.enoca.challenge.entity.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+    DataResult<List<Employee>> findAll();
+
+    DataResult<Employee> findById(int theId);
+
+    DataResult<Employee> save(Employee employee);
+
+    DataResult<String> deleteById(int id);
 }
