d153f6587b08f97353fdcfbcfa92de39a043b129
Rafet Ersoy <63067602+RafetJinx@users.noreply.github.com>
2023-09-16 22:37:32 +0300

    added services implementations

--- src/main/java/com/enoca/challenge/service/concretes/CompanyServiceImpl.java
+++ src/main/java/com/enoca/challenge/service/concretes/CompanyServiceImpl.java
@@ -1,4 +1,70 @@
 package com.enoca.challenge.service.concretes;
 
-public class CompanyServiceImpl {
+import com.enoca.challenge.core.utilities.results.dataResult.DataResult;
+import com.enoca.challenge.core.utilities.results.dataResult.error.ErrorDataResult;
+import com.enoca.challenge.core.utilities.results.dataResult.success.SuccessDataResult;
+import com.enoca.challenge.dao.CompanyRepository;
+import com.enoca.challenge.entity.Company;
+import com.enoca.challenge.service.abstracts.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CompanyServiceImpl implements CompanyService {
+
+    private CompanyRepository companyRepository;
+
+
+    @Autowired
+    public CompanyServiceImpl(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    @Override
+    public DataResult<List<Company>> findAll() {
+        List<Company> companies = companyRepository.findAllByOrderByIdAsc();
+
+        if(companies == null) {
+            return new ErrorDataResult<>("Companies cannot be retrieved from the database. Commpanies list is null", null);
+        }
+
+        if(companies.isEmpty()){
+            return new ErrorDataResult<>("Companies cannot be retrieved from the database. Commpanies list is empty", null);
+        }
+
+        return new SuccessDataResult<>("All companies were successfully retrieved from the database", companies);
+    }
+
+    @Override
+    public DataResult<Company> findById(int id) {
+        int companyCount = (int) companyRepository.count();
+
+        if(id > companyCount){
+            return new ErrorDataResult<>("Company cannot be retrieved from the database. Company id is greater than the number of companies or company deleted", null);
+        }
+
+        Company company = companyRepository.findById(id).orElse(null);
+
+        if(company == null) {
+            return new ErrorDataResult<>("Company cannot be retrieved from the database. Company is null", null);
+        }
+
+        return new SuccessDataResult<>("Company was successfully retrieved from the database", company);
+    }
+
+    @Override
+    public DataResult<Company> save(Company company) {
+        companyRepository.save(company);
+
+        return new SuccessDataResult<>("Company was successfully saved to the database", company);
+    }
+
+    @Override
+    public DataResult<String> deleteById(int id) {
+        companyRepository.deleteById(id);
+
+        return new SuccessDataResult<>("Company was successfully deleted from the database", null);
+    }
 }
--- src/main/java/com/enoca/challenge/service/concretes/EmployeeServiceImpl.java
+++ src/main/java/com/enoca/challenge/service/concretes/EmployeeServiceImpl.java
@@ -1,4 +1,70 @@
 package com.enoca.challenge.service.concretes;
 
-public class EmployeeServiceImpl {
+import com.enoca.challenge.core.utilities.results.dataResult.DataResult;
+import com.enoca.challenge.core.utilities.results.dataResult.error.ErrorDataResult;
+import com.enoca.challenge.core.utilities.results.dataResult.success.SuccessDataResult;
+import com.enoca.challenge.dao.EmployeeRepository;
+import com.enoca.challenge.entity.Employee;
+import com.enoca.challenge.service.abstracts.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public DataResult<List<Employee>> findAll() {
+        List<Employee> employees = employeeRepository.findAllByOrderByIdAsc();
+
+        if(employees == null) {
+            return new ErrorDataResult<>("Employees cannot be retrieved from the database. Employees list is null", null);
+        }
+
+        if(employees.isEmpty()){
+            return new ErrorDataResult<>("Employees cannot be retrieved from the database. Employees list is empty", null);
+        }
+
+        return new SuccessDataResult<>("All employees were successfully retrieved from the database", employees);
+    }
+
+    @Override
+    public DataResult<Employee> findById(int id) {
+        int employeeCount = (int) employeeRepository.count();
+
+        if(id > employeeCount){
+            return new ErrorDataResult<>("Employee cannot be retrieved from the database. Employee id is greater than the number of employees or employee deleted", null);
+        }
+
+        Optional<Employee> employee = employeeRepository.findById(id);
+
+        if(employee == null) {
+            return new ErrorDataResult<>("Employee cannot be retrieved from the database. Employee is null", null);
+        }
+
+        return new SuccessDataResult<>("Employee was successfully retrieved from the database", employee);
+    }
+
+    @Override
+    public DataResult<Employee> save(Employee employee) {
+        employeeRepository.save(employee);
+
+        return new SuccessDataResult<>("Employee was successfully saved to the database", employee);
+    }
+
+    @Override
+    public DataResult<String> deleteById(int id) {
+        employeeRepository.deleteById(id);
+
+        return new SuccessDataResult<>("Employee was successfully deleted from the database", null);
+    }
 }
